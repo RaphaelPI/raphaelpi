6a47739563341171aa8c100bfa9efc94b2afd85e
Raphael pi <pi.raph@gmail.com>
2019-07-04 18:34:29 +0200

    langue

--- src/pages/index.js
+++ src/pages/index.js
@@ -87,7 +87,7 @@ class HomeIndex extends React.Component {
           <section id="one">
             <div>
               <p style={{color: "#333"}}>
-              Passionné par le code depuis toujours, je suis un développeur front-end accompli ayant une expérience partagée entre le travail d'équipe au sein d'une entreprise et le travail en freelance.
+              Je suis un développeur front-end accompli ayant une expérience partagée entre le travail d'équipe au sein d'une entreprise et le travail en freelance.
               </p>
               <p style={{color: "#333"}}>
               Depuis plus de 10 ans, je suis toujours aussi séduit par ce métier qui permet de toujours se remettre en question, faire des rencontres, apprendre et faire évoluer mes compétences au rythme de l'apparition de nouvelles technologies.
